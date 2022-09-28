c206ce45c81e826fb7629e1b1565b364f5a9c78c
boretkiterie <99145013+boretkiterie@users.noreply.github.com>
2022-09-28 17:04:38 +0300

    changed file path
    changed the images path

---
+++ data.js
@@ -0,0 +1,32 @@
+const characterData = {
+    hero: {
+        name: "Wizard",
+        avatar: "imgs/wizard.png",
+        health: 60,
+        diceCount: 3,
+        currentDiceScore: []
+    },
+    orc: {
+        name: "Orc",
+        avatar: "imgs/orc.png",
+        health: 30,
+        diceCount: 1,
+        currentDiceScore: []
+    },
+    demon: {
+        name: "Demon",
+        avatar: "imgs/demon.png",
+        health: 25,
+        diceCount: 2,
+        currentDiceScore: []
+    },
+    goblin: {
+        name: "Goblin",
+        avatar: "imgs/goblin.png",
+        health: 20,
+        diceCount: 3,
+        currentDiceScore: []
+    }
+}
+
+export default characterData
